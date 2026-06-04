bd13d07d17fb660237d712596f2876152c769c9b
Saptarshi Dey <dey7.kol@gmail.com>
2026-06-04 23:33:37 +0530

    creating user structure and user dao service

---
+++ src/main/java/com/example/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,19 @@
+package com.example.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam",LocalDate.now().minusYears(30)));
+        users.add(new User(1, "Eve",LocalDate.now().minusYears(25)));
+        users.add(new User(1, "Jim",LocalDate.now().minusYears(35)));
+    }
+    
+}
