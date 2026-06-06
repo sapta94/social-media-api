534aeebc130b161b8882706dd9292f588851d782
Saptarshi Dey <dey7.kol@gmail.com>
2026-06-06 23:44:57 +0530

    adding post api to add user and send back location in header

--- src/main/java/com/example/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/example/restful_web_services/user/UserDaoService.java
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
-
+    private static int userCOunt = 0;
     static {
-        users.add(new User(1, "Adam",LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eve",LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Jim",LocalDate.now().minusYears(35)));
+        users.add(new User(++userCOunt, "Adam",LocalDate.now().minusYears(30)));
+        users.add(new User(++userCOunt, "Eve",LocalDate.now().minusYears(25)));
+        users.add(new User(++userCOunt, "Jim",LocalDate.now().minusYears(35)));
     }
 
     public List<User> findAll(){
@@ -24,6 +24,12 @@ public class UserDaoService {
     public User findOne(int id){
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().get();
+    }
+
+    public User save(User user){
+        user.setId(++userCOunt);
+        users.add(user);
+        return user;
     }
     
 }
--- src/main/java/com/example/restful_web_services/user/UserResource.java
+++ src/main/java/com/example/restful_web_services/user/UserResource.java
@@ -1,10 +1,15 @@
 package com.example.restful_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -22,5 +27,11 @@ public class UserResource {
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
+    }
+    @PostMapping(path="/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User newUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
